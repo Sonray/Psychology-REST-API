1e4fa89d96c0337583dbfba518a540b88f67831f
David Okwacha <osweredavid@gmail.com>
2020-11-04 12:07:06 +0300

    Registered models in the admin site

--- psychology/admin.py
+++ psychology/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile_User, Post, Comment
 
 # Register your models here.
+
+admin.site.Register(Profile_User)
+admin.site.Register(Post)
+admin.site.Register(Comment)
