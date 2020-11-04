009d0e803e43ffa91ef07891259b669bcefe5afd
David Okwacha <osweredavid@gmail.com>
2020-11-04 07:51:32 +0300

    made profile and post models

--- psychology/models.py
+++ psychology/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model)
+    Profile_pic = models.ImageField(upload_to = 'images/')
+    User_bio = models.TextField()
+    User_email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
+
+
+class Post(models.Model)
+    comment = models.TextField()
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    profile_id = models.ForeignKey(User, on_delete=models.CASCADE,)
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE,)
